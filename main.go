142e665fa6c222a727c16c1310cc1f3c323dda87
sharat789 <sharat157@gmail.com>
2024-12-16 10:19:05 +0200

    refactored code and routes

--- main.go
+++ main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sharat789/example-orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
-
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to lister/serve", err)
+		fmt.Println("failed to start", err)
 	}
-	}
-
-	func basicHandler(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello, world!"))
-	}
+}
